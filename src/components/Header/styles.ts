d1a272e60668434e115639ce02018a772230992b
Natannms <agnusnat.nms@gmail.com>
2023-04-17 21:10:09 -0300

    Estado atual da aplicação

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    width: 100%;
+    flex:1;
+    flex-direction: row;
+    justify-content: center;
+`;
+
+export const Logo = styled.Image`
+    width: 46px;
+    height: 55px;
+`;
